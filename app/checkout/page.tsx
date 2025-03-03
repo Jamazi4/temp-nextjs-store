65aa9234e673db06b39006fa88083e1c7bf3b6d0
Jakub Zimnoch <j.m.zimnoch@gmail.com>
2025-03-03 10:41:49 +0100

    refactor checkout to wrap child using useSearchParams() in a suspense component, now properly

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { Suspense } from "react";
+import { Suspense, useCallback } from "react";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
-import React, { useCallback } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   EmbeddedCheckoutProvider,
@@ -13,9 +12,8 @@ const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
 );
 
-function CheckoutPage() {
+function CheckoutContent() {
   const searchParams = useSearchParams();
-
   const orderId = searchParams.get("orderId");
   const cartId = searchParams.get("cartId");
 
@@ -25,16 +23,22 @@ function CheckoutPage() {
       cartId,
     });
     return response.data.clientSecret;
-  }, []);
+  }, [orderId, cartId]);
 
   const options = { fetchClientSecret };
 
   return (
+    <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+      <EmbeddedCheckout />
+    </EmbeddedCheckoutProvider>
+  );
+}
+
+function CheckoutPage() {
+  return (
     <div id="checkout">
-      <Suspense>
-        <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-          <EmbeddedCheckout />
-        </EmbeddedCheckoutProvider>
+      <Suspense fallback={<div>Loading checkout...</div>}>
+        <CheckoutContent />
       </Suspense>
     </div>
   );
